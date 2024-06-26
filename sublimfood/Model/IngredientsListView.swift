fc4cf7ce22f29f8ebca3147317b5ecfd82c098c8
Tarek Noubli <tareknoubli@gmail.com>
2024-06-26 15:21:54 +0200

    RecipeModel

---
+++ sublimfood/Model/IngredientsListView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientsListView.swift
+//  sublimfood
+//
+//  Created by Tarek Noubli on 26/06/2024.
+//
+
+import SwiftUI
+
+struct IngredientsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientsListView()
+}
