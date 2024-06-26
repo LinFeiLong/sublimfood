ebf304f72a995b56596e77764a604029e0384583
Fernand Lime <fernand.lime@gmail.com>
2024-06-26 13:15:40 +0200

    Add TitleView

---
+++ sublimfood/Components/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  sublimfood
+//
+//  Created by Fernand LIME on 26/06/2024.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
