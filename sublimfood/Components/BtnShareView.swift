04d13f4b8e4248fe31e2ffead549e7f8ea1791d3
Fernand Lime <fernand.lime@gmail.com>
2024-06-26 15:53:40 +0200

    Buttons
    (cherry picked from commit ab55823120240e01ca5200c6463f8f322b9df597)

--- sublimfood/Components/BtnShareView.swift
+++ sublimfood/Components/BtnShareView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct BtnShareView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Button(action: {
+                //shareContent()
+            })  {
+                HStack {
+                    
+                    Image(systemName: "square.and.arrow.up")
+                        .resizable()
+                        .frame(width: 20,height:20)
+                        .foregroundColor(.white)
+                    Text ("Partager")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(10)
+            }
+            Spacer()
+        }
     }
 }
 
--- sublimfood/Components/SharemyFavoriteView.swift
+++ sublimfood/Components/SharemyFavoriteView.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct SharemyFavoriteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Button(action: {
+                //shareContent()
+            })  {
+                HStack {
+                    
+                    
+                    Text ("Ajouter aux Favoris")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 20,height:20)
+                        .foregroundColor(.white)
+                }
+                
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(10)
+            }
+            Spacer()
+        }
     }
 }
+
+#Preview {
+    BtnShareView()
+}
+
 
 #Preview {
     SharemyFavoriteView()
