185d34a5d02e5638074070e26bec32f93e752994
Enzo <ribeiroribeiro50@yahoo.fr>
2024-06-26 15:34:02 +0200

    titleview

--- sublimfood/Components/TitleView.swift
+++ sublimfood/Components/TitleView.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
-struct TitleView: View {
+struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            //                HStack {
+            //                    CategoryView(title: "Congélation", imageName: "congelation", subcategories: ["Légumes variés", "Glaces de petits pois", "Sorbet de framboises"])
+            //                }
+            //                HStack {
+            //                    CategoryView(title: "Recyclage", imageName: "recyclage", subcategories: ["Composte", "Bouillons", "Vermicompost"])
+            //                }
+            //                HStack {
+            //                    CategoryView(title: "Cosmétiques", imageName: "cosmetiques", subcategories: [])
+            //                }
+        }
     }
 }
 
-#Preview {
-    TitleView()
+struct CategoryView: View {
+    let title: String
+    let imageName: String
+    let subcategories: [String]
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.black)
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(5)
+                .padding(.bottom, 5)
+            
+            ForEach(subcategories, id: \.self) { subcategory in
+                HStack {
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    Text(subcategory)
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                }
+                .padding(.bottom, 5)
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
 }
