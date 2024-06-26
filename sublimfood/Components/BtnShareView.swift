5001041e7c5bcb20a957b0c167d0377c5f2686a9
Aboudou <imrane.aboudou01@gmail.com>
2024-06-26 15:37:17 +0200

    Favorite Button &  Share Button
    (cherry picked from commit cc4e898060d76fb4e96fee96944cd8cb5460c825)

---
+++ sublimfood/Components/BtnShareView.swift
@@ -0,0 +1,18 @@
+//
+//  BtnShareView.swift
+//  sublimfood
+//
+//  Created by Apprenant 48 on 26/06/2024.
+//
+
+import SwiftUI
+
+struct BtnShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BtnShareView()
+}
---
+++ sublimfood/Components/SharemyFavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  SharemyFavoriteView.swift
+//  sublimfood
+//
+//  Created by Apprenant 48 on 26/06/2024.
+//
+
+import SwiftUI
+
+struct SharemyFavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SharemyFavoriteView()
+}
