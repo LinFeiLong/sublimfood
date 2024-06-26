188976397cb14ae970350bf0a9c60628a121cfe3
Fernand Lime <fernand.lime@gmail.com>
2024-06-26 10:12:00 +0200

    First commit

---
+++ sublimfood/sublimfoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  sublimfoodApp.swift
+//  sublimfood
+//
+//  Created by Fernand LIME on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct sublimfoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
