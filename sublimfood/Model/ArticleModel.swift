4e88298ba9b7c6df8aae59752377a5f563cfd153
Sourinda SIVILAY <s.sivilay@live.fr>
2024-06-27 14:59:11 +0200

    refactor: move articlesModel to ArticleModel

--- sublimfood/Model/ArticleModel.swift
+++ sublimfood/Model/ArticleModel.swift
@@ -21,3 +21,17 @@ struct ArticleModel: Identifiable {
         self.content = content
     }
 }
+
+var caption: String = "Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit"
+
+var title: String = "Boulettes de Pommes de Terre"
+
+var image: String = "boulettes_pdt"
+
+var content: String = "Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit"
+
+var articlesModel: [ArticleModel] = [
+    ArticleModel(title: title, image: image, caption: caption, content: content ),
+    ArticleModel(title: title, image: image, caption: "", content: ""),
+    ArticleModel(title: title, image: image)
+]
