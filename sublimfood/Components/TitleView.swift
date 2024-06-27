257dfa53c1c73c1ae076121f9f4c5fe7d7828254
Enzo <ribeiroribeiro50@yahoo.fr>
2024-06-27 09:39:40 +0200

    Titleview

--- sublimfood/Components/TitleView.swift
+++ sublimfood/Components/TitleView.swift
@@ -7,53 +7,27 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TitleView: View {
+    let title: String
+    var color:Color
+    
     var body: some View {
-        VStack {
-            //                HStack {
-            //                    CategoryView(title: "Congélation", imageName: "congelation", subcategories: ["Légumes variés", "Glaces de petits pois", "Sorbet de framboises"])
-            //                }
-            //                HStack {
-            //                    CategoryView(title: "Recyclage", imageName: "recyclage", subcategories: ["Composte", "Bouillons", "Vermicompost"])
-            //                }
-            //                HStack {
-            //                    CategoryView(title: "Cosmétiques", imageName: "cosmetiques", subcategories: [])
-            //                }
+        VStack(alignment: .leading){
+            Text(title)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(.black)
+            Rectangle()
+                .fill(color)
+                .frame(width:20,height:5)
         }
     }
 }
 
-struct CategoryView: View {
-    let title: String
-    let imageName: String
-    let subcategories: [String]
-    
-    var body: some View {
-        VStack {
-            Text(title)
-                .font(.headline)
-                .foregroundColor(.black)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(5)
-                .padding(.bottom, 5)
-            
-            ForEach(subcategories, id: \.self) { subcategory in
-                HStack {
-                    Image(imageName)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
-                    Text(subcategory)
-                        .font(.subheadline)
-                        .foregroundColor(.black)
-                }
-                .padding(.bottom, 5)
-            }
-        }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: 5)
+#Preview {
+    VStack(alignment: .leading){
+        TitleView(title: "Congelation", color: .orange)
+        TitleView(title: "Recyclage", color: .orange)
+        TitleView(title: "Cosmetique", color: .orange)
     }
+    
 }
