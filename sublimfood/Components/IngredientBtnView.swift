5130d2106d1cd0832d524220e96d5cfaacf55e4d
Tarek Noubli <tareknoubli@gmail.com>
2024-06-27 15:17:38 +0200

    Create a new IngredientBtnView

---
+++ sublimfood/Components/IngredientBtnView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientBtnView.swift
+//  sublimfood
+//
+//  Created by Tarek Noubli on 27/06/2024.
+//
+
+import SwiftUI
+
+struct IngredientBtnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientBtnView()
+}
