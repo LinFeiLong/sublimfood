a4feda8e4f51fa183009553ba04771c70d37df8c
Tarek Noubli <tareknoubli@gmail.com>
2024-06-27 14:28:56 +0200

    TabView extraction

---
+++ sublimfood/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  sublimfood
+//
+//  Created by Tarek Noubli on 27/06/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
