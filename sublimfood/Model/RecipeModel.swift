bd80de3c0652a2d51618221131a0bbffc7406c8a
Sourinda SIVILAY <s.sivilay@live.fr>
2024-06-27 16:41:06 +0200

    feat: create RecipeDetailView

--- sublimfood/Model/RecipeModel.swift
+++ sublimfood/Model/RecipeModel.swift
@@ -15,6 +15,22 @@ struct RecipeModel: Identifiable {
     let steps: [String]
 }
 
+var recipeModel = RecipeModel(
+    title: "Boulettes de Patates",
+    image: "boulettes_pdt",
+    ingredients: ["1kg PDT",
+                  "1 oeuf",
+                  "2 blancs d'oeuf",
+                  "4 cas chapelure",
+                  "150g boeu d'Auvergne",
+                  "huile de friture",
+                  "sel",
+                  "poivre"],
+    steps: ["Faites cuire les pommes de terre à la vapeur, épluchez-les et écrasez-les en purée.",
+            "Salez, poivrez et incorporez l'oeuf entier à la purée refroidie.",
+            "Formez des boulettes dans lequelles vous insérerez une noisette de bleu d'Auvergne.",
+            "Passez-les dans les 2 blancs d'oeufs légèrement battus, puis dans la chapelure.",
+            "Laissez reposer 30 min avant de les frires 5 min dans l'huile bien chaude"])
 struct Recipes {
     static let all = [
         RecipeModel(title: "Gazpacho de tomates",
---
+++ sublimfood/Views/RecipeDetailView.swift
@@ -0,0 +1,67 @@
+//
+//  RecipeDetailView.swift
+//  sublimfood
+//
+//  Created by Sourinda Sivilay on 27/06/2024.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var recipe: RecipeModel
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                Image("boulettes_pdt")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 300)
+                
+                VStack(spacing: 20) {
+                    VStack {
+                        ButtonView(label: "Ajouter aux favoris", icon: "heart", fontColor: .white, color: .orange, borderColor: .orange)
+                        
+                        ButtonView(label: "Partager la recette", icon: "square.and.arrow.up", fontColor: .orange, color: .white, borderColor: .orange)
+                    }
+                    
+                    Text("Ingrédients")
+                        .font(.title)
+                        .bold()
+                    
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        ForEach(recipe.ingredients, id: \.self) { ingredient in
+                            IngredientButtonView(action: {}, imageName: "tomato", label: ingredient)
+                        }
+                    }
+                    .padding()
+                    
+                    Text("Préparation")
+                        .font(.title)
+                        .bold()
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(Array(recipe.steps.enumerated()), id: \.element) { index, step in
+                            Text("ETAPE \(index + 1)")
+                                .bold()
+                                .font(.title2)
+                            Text(step)
+                        }
+                    }
+                    .padding()
+                }
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    RecipeDetailView(recipe: recipeModel)
+}
