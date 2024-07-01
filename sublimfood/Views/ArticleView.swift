01e7f382e6f0fb46858aab3a6097b4f852260094
Sourinda SIVILAY <s.sivilay@live.fr>
2024-07-01 11:32:38 +0200

    fix: remove other articles in ArticleView

--- sublimfood/Views/ArticleView.swift
+++ sublimfood/Views/ArticleView.swift
@@ -9,59 +9,41 @@ import SwiftUI
 
 struct ArticleView: View {
     var article: ArticleModel
-    var otherArticles: [ArticleModel]
     
     var body: some View {
-        NavigationStack {
-            ScrollView(.vertical, showsIndicators: false) {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                Image(article.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 300)
+                    .frame(maxWidth: UIScreen.main.bounds.width)
+                    .clipped()
+                
+                
                 VStack {
-                    Image(article.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(height: 300)
-                        .frame(maxWidth: UIScreen.main.bounds.width)
-                        .clipped()
-                    
-                    
-                    VStack {
-                        VStack(alignment: .leading) {
-                            Text(article.caption)
-                                .font(.caption)
-                                .padding(.vertical, 10)
-                            
-                            Text(article.title)
-                                .font(.title2)
-                                .bold()
-                                .padding(.bottom, 10)
-                            
-                            Text(article.content)
-                            
-                            Text("Autres astuces")
-                                .font(.title2)
-                                .bold()
-                                .padding(.vertical, 10)
-                        }
-                        .padding()
+                    VStack(alignment: .leading) {
+                        Text(article.caption)
+                            .font(.caption)
+                            .padding(.vertical, 10)
                         
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack {
-                                ForEach(Array(otherArticles.enumerated()), id: \.element.id) { index, article in
-                                    NavigationLink {
-                                        ArticleView(article: articlesModel[index], otherArticles: articlesModel)
-                                    } label : {
-                                        RecipeCardView(image: article.image, title: article.title, displayCircleHeart: false, isHeartFilled: false)
-                                    }
-                                }.navigationTitle("Astuces")
-                            }
-                        }
+                        Text(article.title)
+                            .font(.title2)
+                            .bold()
+                            .padding(.bottom, 10)
+                        
+                        Text(article.content)
                     }
+                    .padding()
+                    
                 }
             }
+            
             
         }
     }
 }
 
 #Preview {
-    ArticleView(article: articlesModel[0], otherArticles: articlesModel)
+    ArticleView(article: articlesModel[0])
 }
