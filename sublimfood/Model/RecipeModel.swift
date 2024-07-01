15953b52081b65f4c141fb58cb96503a3b7a8e08
Tarek Noubli <tareknoubli@gmail.com>
2024-07-01 12:02:06 +0200

    add typeOfRecipe enum

--- sublimfood/Model/RecipeModel.swift
+++ sublimfood/Model/RecipeModel.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+enum TypeOfRecipe {
+    case cook
+    case upcycling
+    case other
+}
+
 struct RecipeModel: Identifiable, Encodable & Decodable {
     var id = UUID()
     let title: String
