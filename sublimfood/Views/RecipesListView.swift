3186cb9ee6cdc310f7a5c3d78306191a34948dc5
Tarek Noubli <tareknoubli@gmail.com>
2024-07-02 14:20:44 +0200

    Refactor RecipesListView with RecipesSectionViews

--- sublimfood/Views/RecipesListView.swift
+++ sublimfood/Views/RecipesListView.swift
@@ -8,20 +8,14 @@
 import SwiftUI
 
 struct RecipesListView: View {
+
     var ingredient: String
     @EnvironmentObject var favoritesManager: FavoritesManager
-    var recipes: [RecipeModel] {
-        Recipes.all.filter { $0.ingredients.contains(ingredient) }
-    }
-    var cookRecipes: [RecipeModel] {
-        recipes.filter { $0.typeOfRecipe == .cook }
-    }
-    var upCyclingRecipes: [RecipeModel] {
-        recipes.filter { $0.typeOfRecipe == .upcycling }
-    }
-    var otherRecipes: [RecipeModel] {
-        recipes.filter { $0.typeOfRecipe == .other }
-    }
+    var recipes: [RecipeModel] { Recipes.all.filter { $0.ingredients.contains(ingredient) } }
+    var cookRecipes: [RecipeModel] { recipes.filter { $0.typeOfRecipe == .cook } }
+    var upCyclingRecipes: [RecipeModel] { recipes.filter { $0.typeOfRecipe == .upcycling } }
+    var otherRecipes: [RecipeModel] { recipes.filter { $0.typeOfRecipe == .other } }
+
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
@@ -38,95 +32,62 @@ struct RecipesListView: View {
                     .frame(height: BTN_HEIGHT)
                     .padding(.bottom, 20)
                     if !cookRecipes.isEmpty {
-                        TitleView(title: "Recettes alimentaires", color: .cyan)
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack (spacing: 20) {
-                                ForEach(cookRecipes) {recipe in
-                                    ZStack {
-                                        NavigationLink {
-                                            RecipeDetailView(recipe: recipe)
-                                        } label: {
-                                            RecipeCardView(image: recipe.image,
-                                                           title: recipe.title,
-                                                           displayCircleHeart: false,
-                                                           isHeartFilled: false)
-                                        }
-                                        Button(action: {
-                                            withAnimation {
-                                                favoritesManager.toggleFavorite(recipe)
-                                            }
-                                        }) {
-                                            CircleHeartFillView(
-                                                isHeartFilled: favoritesManager.isFavorite(recipe)
-                                            )
-                                        }
-                                        .offset(x: 60, y: -80)
-                                    }
-                                }
-                            }.padding(15)
-                        }
+                        RecipesSectionView(title: "Recettes alimentaires",
+                                           titleLineColor: .cyan,
+                                           recipes: cookRecipes)
                     }
                     if !upCyclingRecipes.isEmpty {
-                        TitleView(title: "Upcycling", color: .green)
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack (spacing: 20) {
-                                ForEach(upCyclingRecipes) {recipe in
-                                    ZStack {
-                                        NavigationLink {
-                                            RecipeDetailView(recipe: recipe)
-                                        } label: {
-                                            RecipeCardView(image: recipe.image,
-                                                           title: recipe.title,
-                                                           displayCircleHeart: false,
-                                                           isHeartFilled: false)
-                                        }
-                                        Button(action: {
-                                            withAnimation {
-                                                favoritesManager.toggleFavorite(recipe)
-                                            }
-                                        }) {
-                                            CircleHeartFillView(
-                                                isHeartFilled: favoritesManager.isFavorite(recipe)
-                                            )
-                                        }
-                                        .offset(x: 60, y: -80)
-                                    }
-                                    
-                                }
-                            }.padding(15)
-                        }
+                        RecipesSectionView(title: "Upcycling",
+                                           titleLineColor: .green,
+                                           recipes: upCyclingRecipes)
                     }
                     if !otherRecipes.isEmpty {
-                        TitleView(title: "Autres recettes", color: .pink)
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack (spacing: 20) {
-                                ForEach(otherRecipes) {recipe in
-                                    ZStack {
-                                        NavigationLink {
-                                            RecipeDetailView(recipe: recipe)
-                                        } label: {
-                                            RecipeCardView(image: recipe.image, title: recipe.title, displayCircleHeart: false, isHeartFilled: false)
-                                        }
-                                        Button(action: {
-                                            withAnimation {
-                                                favoritesManager.toggleFavorite(recipe)
-                                            }
-                                        }) {
-                                            CircleHeartFillView(
-                                                isHeartFilled: favoritesManager.isFavorite(recipe)
-                                            )
-                                        }
-                                        .offset(x: 60, y: -80)
-                                    }
-                                }.padding(15)
-                            }
-                        }
+                        RecipesSectionView(title: "Autres recettes", 
+                                           titleLineColor: .pink,
+                                           recipes: otherRecipes)
                     }
                 }
             }
             .navigationTitle("Recettes")
             .toolbarBackground(Color.orange, for: .navigationBar)
             .toolbarBackground(.visible, for: .navigationBar)
+        }
+    }
+}
+
+struct RecipesSectionView: View {
+    let title: String
+    let titleLineColor: Color
+    let recipes: [RecipeModel]
+    @EnvironmentObject var favoritesManager: FavoritesManager
+    
+    var body: some View {
+        TitleView(title: title, color: titleLineColor)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack (spacing: 20) {
+                ForEach(recipes) { recipe in
+                    ZStack {
+                        NavigationLink {
+                            RecipeDetailView(recipe: recipe)
+                        } label: {
+                            RecipeCardView(image: recipe.image, 
+                                           title: recipe.title,
+                                           displayCircleHeart: false,
+                                           isHeartFilled: false)
+                        }
+                        Button(action: {
+                            withAnimation {
+                                favoritesManager.toggleFavorite(recipe)
+                            }
+                        }) {
+                            CircleHeartFillView(
+                                isHeartFilled: favoritesManager.isFavorite(recipe)
+                            )
+                        }
+                        .offset(x: 60, y: -80)
+                    }
+                }.padding(15)
+            }
         }
     }
 }
