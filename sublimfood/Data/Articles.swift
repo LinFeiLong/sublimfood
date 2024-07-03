222b5cf24876a777f25c6a8e49d30886594a339e
Tarek Noubli <tareknoubli@gmail.com>
2024-07-03 15:38:48 +0200

    Remove of examples

--- sublimfood/Data/Articles.swift
+++ sublimfood/Data/Articles.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-var caption: String = "Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit"
-
-var content: String = "Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit Neque porro quisquam est qui dolorem ipsum quia dolor si amet, consectetur, adipisci velit"
-
-var articlesModel: [ArticleModel] = [
-    ArticleModel(title: "Boulettes de Pomme de Terre", image: "boulettes_pdt", caption: caption, content: content, type: .recycle),
-    ArticleModel(title: "Gazpacho de tomates", image: "gazpacho_tomates", caption: caption, content: content, type: .recycle ),
-    ArticleModel(title: "Légumes sautés", image: "legumes_sautes", caption: caption, content: content, type: .recycle ),
-    ArticleModel(title: "Sorbet de framboises", image: "sorbet_framboises", caption: caption, content: content, type: .recycle ),
-]
 struct Articles {
     static let all = [
         ArticleModel(
